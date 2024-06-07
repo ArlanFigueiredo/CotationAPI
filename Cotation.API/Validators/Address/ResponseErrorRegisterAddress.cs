e817328d56c7bb93a7171f7c552535c59096a52a
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-07 12:11:05 -0300

    Credted process of validation data of register of address

---
+++ Cotation.API/Validators/Address/ResponseErrorRegisterAddress.cs
@@ -0,0 +1,23 @@
+﻿using Cotation.Communication.ModelsViews.Requests.Address;
+
+namespace Cotation.API.Validators.Address {
+    public class ResponseErrorRegisterAddress(ValidatorRegisterAddress validator) {
+        private readonly ValidatorRegisterAddress _validator = validator;
+
+        public async Task<List<ResponseError>> GetErrors(RequestAddress request) {
+
+            var validatorAddress = await _validator.ValidateAsync(request);
+
+            var erros = new List<ResponseError>();
+
+            foreach (var error in validatorAddress.Errors) {
+
+                erros.Add(new ResponseError { 
+                    PropertyName = error.PropertyName,
+                    ErrorMessage = error.ErrorMessage,
+                });
+            }
+            return erros;
+        }
+    }
+}
---
+++ Cotation.API/Validators/Address/ValidatorRegisterAddress.cs
@@ -0,0 +1,36 @@
+﻿using Cotation.Communication.ModelsViews.Requests.Address;
+using FluentValidation;
+
+namespace Cotation.API.Validators.Address {
+    public class ValidatorRegisterAddress : AbstractValidator<RequestAddress> {
+
+        [Obsolete]
+        public ValidatorRegisterAddress() {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(a => a.ZipCode)
+                .NotEmpty()
+                .WithMessage("O campo CEP é obrigatorio. Por favor, preencha-o");
+
+            RuleFor(a => a.Road)
+                .NotEmpty()
+                .WithMessage("O campo Rua é obrigatorio. Por favor, preencha-o");
+
+            RuleFor(a => a.NeighBorHood)
+                .NotEmpty()
+                .WithMessage("O campo Bairro é obrigatorio. Por favor, preencha-o");
+
+            RuleFor(a => a.Number)
+                .NotEmpty()
+                .WithMessage("O campo Numero é obrigatorio. Por favor, preencha-o");
+
+            RuleFor(a => a.City)
+                .NotEmpty()
+                .WithMessage("O campo Cidade é obrigatorio. Por favor, preencha-o");
+
+            RuleFor(a => a.State)
+                .NotEmpty()
+                .WithMessage("O campo Estado é obrigatorio. Por favor, preencha-o");
+        }
+    }
+}
