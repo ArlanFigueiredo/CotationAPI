a5a29a743dcbfb8c7bd3f0be22ecaab7abe1123a
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-07 10:15:07 -0300

    Creating controller of Get all company

---
+++ Cotation.API/Controllers/Company/GetAllCompanyController.cs
@@ -0,0 +1,25 @@
+﻿using Cotation.Application.Services.SCompany;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cotation.API.Controllers.Company {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetAllCompanyController(GetAllCompanyService getAllCompanyService) : ControllerBase {
+        private readonly GetAllCompanyService _getAllCompanyService = getAllCompanyService;
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> GetAll() {
+            var service = _getAllCompanyService;
+
+            try {
+                var result = await service.Execute();
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
