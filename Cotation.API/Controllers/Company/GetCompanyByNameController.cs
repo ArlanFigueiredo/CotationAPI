018ccd0265922db614367da281ddf180f78db8cd
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-07 10:49:55 -0300

    Creating controller of Get Company By name

---
+++ Cotation.API/Controllers/Company/GetCompanyByNameController.cs
@@ -0,0 +1,25 @@
+﻿using Cotation.Application.Services.SCompany;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cotation.API.Controllers.Company {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetCompanyByNameController(GetCompanyByNameService getCompanyByNameService) : ControllerBase {
+        private readonly GetCompanyByNameService _getCompanyByNameService = getCompanyByNameService;
+
+        [HttpGet("/company/name")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> GetByName([FromBody] string name) {
+            var service = _getCompanyByNameService;
+
+            try {
+                var result = await service.Execute(name);
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
