6cb12b53e303dd126bf3d4580f11eb68e158ff5d
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-08 20:46:33 -0300

    Creating service get item by id

---
+++ Cotation.Application/Services/SItem/GetItemByIdService.cs
@@ -0,0 +1,19 @@
+﻿using Cotation.Communication.ModelsViews.Responses.Item;
+using Cotation.Infrastructure.Repositories.RepositoryItem;
+
+namespace Cotation.Application.Services.SItem {
+    public class GetItemByIdService(IItemsRepository itemsRepository) {
+        private readonly IItemsRepository _itemsRepository = itemsRepository;
+
+        public async Task<ResponseItem> Execute(Guid id) {
+            var item = await _itemsRepository.GetById(id) ?? throw new Exception("Item não existe.");
+
+            return new ResponseItem {
+                Id = item.Id,
+                CompanyId = item.CompanyId,
+                ProductId = item.ProductId,
+                Quantity = item.Quantity,
+            };
+        }
+    }
+}
