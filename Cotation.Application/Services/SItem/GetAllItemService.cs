bee12043866a07187ed8a4c9ca653456f0cbb07c
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-08 20:42:39 -0300

    Creating serve get all items

---
+++ Cotation.Application/Services/SItem/GetAllItemService.cs
@@ -0,0 +1,26 @@
+﻿using Cotation.Communication.ModelsViews.Responses.Item;
+using Cotation.Infrastructure.Repositories.RepositoryItem;
+
+namespace Cotation.Application.Services.SItem {
+    public class GetAllItemService(IItemsRepository itemsRepository) {
+        private readonly IItemsRepository _itemsRepository = itemsRepository;
+
+        public async Task<List<ResponseItem>> Execute() {
+
+            var item = await _itemsRepository.GetAll();
+
+            var list = new List<ResponseItem>();
+
+            foreach (var itemItem in item) {
+                list.Add(new ResponseItem {
+                    Id = itemItem.Id,
+                    CompanyId = itemItem.CompanyId,
+                    ProductId = itemItem.ProductId,
+                    Quantity = itemItem.Quantity,
+                });
+            }
+
+            return list;
+        }
+    }
+}
