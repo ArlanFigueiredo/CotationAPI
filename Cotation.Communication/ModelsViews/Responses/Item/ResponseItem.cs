b6f14b2c70733230d08b2c99342a4e5c20658efe
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-08 16:03:00 -0300

    Update modelview response

--- Cotation.Communication/ModelsViews/Responses/Item/ResponseItem.cs
+++ Cotation.Communication/ModelsViews/Responses/Item/ResponseItem.cs
@@ -1,8 +1,19 @@
-﻿namespace Cotation.Communication.ModelsViews.Responses.Item {
+﻿using Cotation.Communication.DTOS.ItemDTO;
+
+namespace Cotation.Communication.ModelsViews.Responses.Item {
     public class ResponseItem {
         public Guid Id { get; set; }  
         public Guid CompanyId { get; set; }
         public Guid ProductId { get; set; }
         public double Quantity { get; set; }
+
+        public ResponseItem() { }
+
+        public ResponseItem(DTOItem dTOItem) {
+            Id = dTOItem.Id;
+            CompanyId = dTOItem.CompanyId;
+            ProductId = dTOItem.ProductId;
+            Quantity = dTOItem.Quantity;
+        }
     }
 }
