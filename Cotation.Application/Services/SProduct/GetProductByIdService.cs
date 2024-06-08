165bc350ccb9e0436e57ec564c8ba36f7dc7696f
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-08 18:57:13 -0300

    Creating service of get product by id

---
+++ Cotation.Application/Services/SProduct/GetProductByIdService.cs
@@ -0,0 +1,20 @@
+﻿using Cotation.Communication.ModelsViews.Responses.Product;
+using Cotation.Infrastructure.Repositories.RepositoryProduct;
+
+namespace Cotation.Application.Services.SProduct {
+    public class GetProductByIdService(IProductRepository productRepository) {
+        private readonly IProductRepository _productRepository = productRepository;
+
+        public async Task<ResponseProduct> Execute(Guid id) {
+            var product = await _productRepository.GetById(id) ?? throw new Exception("Produto não encontrado.");
+
+            return new ResponseProduct {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Category = product.Category,
+                PriceUnit = product.PriceUnit,
+            };
+        }
+    }
+}
