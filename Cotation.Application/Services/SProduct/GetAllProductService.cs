a3da3fcee36ef393895a601372aa1d29b5c67c35
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-08 19:05:19 -0300

    Creting service of get all products

---
+++ Cotation.Application/Services/SProduct/GetAllProductService.cs
@@ -0,0 +1,26 @@
+﻿using Cotation.Communication.ModelsViews.Responses.Product;
+using Cotation.Infrastructure.Repositories.RepositoryProduct;
+
+namespace Cotation.Application.Services.SProduct {
+    public class GetAllProductService(IProductRepository productRepository) {
+        private readonly IProductRepository _productRepository = productRepository;
+
+        public async Task<List<ResponseProduct>> Execute() {
+
+            var product = await _productRepository.GetAll();
+
+            var listProduct = new List<ResponseProduct>();
+
+            foreach (var products in product) {
+                listProduct.Add(new ResponseProduct {
+                    Id = products.Id,
+                    Name = products.Name,
+                    Description = products.Description,
+                    Category = products.Category,
+                    PriceUnit = products.PriceUnit,
+                });
+            }
+            return listProduct;
+        }
+    }
+}
