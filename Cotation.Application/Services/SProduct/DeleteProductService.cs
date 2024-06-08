ef9393c37b2fabd3debcb7bbb729c29c517fb8ae
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-08 18:54:01 -0300

    Creating service of delete product

---
+++ Cotation.Application/Services/SProduct/DeleteProductService.cs
@@ -0,0 +1,13 @@
+﻿using Cotation.Infrastructure.Repositories.RepositoryProduct;
+
+namespace Cotation.Application.Services.SProduct {
+    public class DeleteProductService(IProductRepository productRepository) {
+        private readonly IProductRepository _productRepository = productRepository;
+        private readonly string MessageSuccess = "Produto deletado com sucesso.";
+        public async Task<string> Execute(Guid id) {
+            var product = await _productRepository.GetById(id) ?? throw new Exception("Produto não encontrado");
+            await _productRepository.Delete(product);
+            return MessageSuccess;
+        }
+    }
+}
