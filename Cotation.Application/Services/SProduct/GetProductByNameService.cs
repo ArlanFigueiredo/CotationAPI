4345c4c00d3e033c3356d47803d1e15e9386131b
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-08 18:59:32 -0300

    Creting service of get product by name

---
+++ Cotation.Application/Services/SProduct/GetProductByNameService.cs
@@ -0,0 +1,20 @@
+﻿using Cotation.Communication.ModelsViews.Responses.Product;
+using Cotation.Infrastructure.Repositories.RepositoryProduct;
+
+namespace Cotation.Application.Services.SProduct {
+    public class GetProductByNameService(IProductRepository productRepository) {
+        private readonly IProductRepository _productRepository = productRepository;
+
+        public async Task<ResponseProduct> Execute(string name) {
+            var product = await _productRepository.GetByName(name) ?? throw new Exception("Produto não existe.");
+
+            return new ResponseProduct {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Category = product.Category,
+                PriceUnit = product.PriceUnit,
+            };
+        }
+    }
+}
