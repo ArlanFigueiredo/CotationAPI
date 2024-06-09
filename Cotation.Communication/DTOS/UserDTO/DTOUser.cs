b6f457c1d918a18b4749f9ee970c4e94573c9bd2
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-09 17:03:07 -0300

    update dtouser whit constructor for send params

--- Cotation.Communication/DTOS/UserDTO/DTOUser.cs
+++ Cotation.Communication/DTOS/UserDTO/DTOUser.cs
@@ -1,4 +1,6 @@
-﻿namespace Cotation.Communication.DTOS.UserDTO {
+﻿using Cotation.Communication.ModelsViews.Requests.User;
+
+namespace Cotation.Communication.DTOS.UserDTO {
     public class DTOUser {
         public Guid Id { get; set; }
         public string Name { get; set; }
@@ -7,5 +9,15 @@
         public string Salt { get; set; }
         public string Role { get; set; }
         public DateTime CreatedAt { get; set; }
+
+        public DTOUser(RequestUser user) {
+            Name = user.Name; 
+            Email = user.Email; 
+            Password = user.Password; 
+            Salt = user.Salt; 
+            Role = user.Role;
+        }
+
+        public DTOUser() { }
     }
 }
