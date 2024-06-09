c3432ed598defda954d1aaabca42300357d35002
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-09 17:23:49 -0300

    Creating utilities of verify hash of password

---
+++ Cotation.Application/Utilities/User/VerifyHashPassword.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace Cotation.Application.Utilities.User {
+    public class VerifyHashPassword {
+        public bool VerifyPasswordHash(string storedHash, string storedSalt, string userPassword) {
+            // Converta o salt armazenado de base64 para bytes
+            byte[] salt = Convert.FromBase64String(storedSalt);
+
+            // Gere o hash da senha inserida
+            byte[] userHashBytes = KeyDerivation.Pbkdf2(
+                password: userPassword,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8
+            );
+
+            // Converta o hash gerado para base64
+            string userHash = Convert.ToBase64String(userHashBytes);
+
+            // Compare os hashes
+            return userHash == storedHash;
+        }
+    }
+}
