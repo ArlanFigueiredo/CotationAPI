266945ba99ed933800b75239245ada3012c2f1f0
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-09 17:22:14 -0300

    Creating utilities for generate token jwt for authenticate

---
+++ Cotation.Application/Utilities/User/GenerateTokenJWT.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Cotation.Application.Utilities.User {
+    public class GenerateTokenJWT {
+        public string GenerateJwtToken(string userId, string email, string userRole) {
+            var key = Encoding.ASCII.GetBytes("dfwesdpomv3ewf23ref23fwfwe23rfwqfdsedf234ff2332faewsdcsad23r32er23qer");
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userId),
+                    new Claim(ClaimTypes.Email, email),
+                    new Claim(ClaimTypes.Role, userRole)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
