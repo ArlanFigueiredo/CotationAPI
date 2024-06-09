edc49ae0947d2ee41cf60b0b9517464f20efbd98
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-09 17:23:17 -0300

    Creating model viewl for request data for login

---
+++ Cotation.Communication/ModelsViews/Requests/User/RequestLoginUser.cs
@@ -0,0 +1,6 @@
+﻿namespace Cotation.Communication.ModelsViews.Requests.User {
+    public class RequestLoginUser {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
