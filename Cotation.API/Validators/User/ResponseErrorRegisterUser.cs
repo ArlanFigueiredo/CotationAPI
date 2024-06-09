61bdd231133aff33a003dceedb43a1783286af0d
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-09 17:48:32 -0300

    Creating validator for created user

---
+++ Cotation.API/Validators/User/ResponseErrorRegisterUser.cs
@@ -0,0 +1,22 @@
+﻿using Cotation.Communication.ModelsViews.Requests.User;
+
+namespace Cotation.API.Validators.User {
+    public class ResponseErrorRegisterUser(ValidatorRegisterUser validator) {
+        private readonly ValidatorRegisterUser _validator = validator;
+
+        public async Task<List<ResponseError>> GetErros(RequestUser request) {
+            var validatorUser = await _validator.ValidateAsync(request);
+
+            var erros = new List<ResponseError>();
+
+            if (!validatorUser.IsValid) {
+
+                foreach (var error in validatorUser.Errors) {
+                    erros.Add(new ResponseError(error.PropertyName, error.ErrorMessage));
+                }
+            }
+
+            return erros;
+        }
+    }
+}
---
+++ Cotation.API/Validators/User/ValidatorRegisterUser.cs
@@ -0,0 +1,24 @@
+﻿using Cotation.Communication.ModelsViews.Requests.User;
+using FluentValidation;
+
+namespace Cotation.API.Validators.User {
+    public class ValidatorRegisterUser : AbstractValidator<RequestUser> {
+
+        [Obsolete]
+        public ValidatorRegisterUser() {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(user => user.Name)
+                .NotEmpty()
+                .WithMessage("O campo Nome é obrigatorio. Por favor, preencha-o.");
+
+            RuleFor(user => user.Email)
+                .NotEmpty()
+                .WithMessage("O campo Email é obrigatorio. Por favor, preencha-o.");
+
+            RuleFor(user => user.Password)
+                .NotEmpty()
+                .WithMessage("O campo Password é obrigatorio. Por favor, preencha-o.");
+        }
+    }
+}
