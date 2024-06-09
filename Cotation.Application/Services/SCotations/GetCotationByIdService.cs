1aef9110a2910bf8650cb4efc28c5d08d8706891
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-09 16:18:19 -0300

    Creating service of get cotation by id

---
+++ Cotation.Application/Services/SCotations/GetCotationByIdService.cs
@@ -0,0 +1,17 @@
+﻿using Cotation.Communication.ModelsViews.Responses.Cotations;
+using Cotation.Infrastructure.Repositories.RepositoryCotations;
+
+namespace Cotation.Application.Services.SCotations {
+    public class GetCotationByIdService(ICotationRepository cotationRepository) {
+        private readonly ICotationRepository _cotationRepository = cotationRepository;
+
+        public async Task<ResponseCotations> Execute(Guid id) {
+            var cotation = await _cotationRepository.GetById(id) ?? throw new Exception("Cotação não existe. Por favor, verifique.");
+            return new ResponseCotations {
+                CompanyId = cotation.CompanyId,
+                UserId = cotation.UserId,
+                Amount = cotation.Amount,
+            };
+        }
+    }
+}
