8ba00128a43566e8050c56e0275293db7fb300be
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-09 16:14:17 -0300

    Creatin serive of delete cotation

---
+++ Cotation.Application/Services/SCotations/DeleteCotationSerivce.cs
@@ -0,0 +1,13 @@
+﻿using Cotation.Infrastructure.Repositories.RepositoryCotations;
+
+namespace Cotation.Application.Services.SCotations {
+    public class DeleteCotationSerivce(ICotationRepository cotationRepository) {
+        private readonly ICotationRepository _cotationRepository = cotationRepository;
+        private readonly string MessageSuccess = "Cotação deletado com sucesso.";
+        public async Task<string> Execute(Guid id) {
+            var cotation = await _cotationRepository.GetById(id) ?? throw new Exception("Cotação não encontrada. Por favor, verifique.");
+            await _cotationRepository.Delete(cotation);
+            return MessageSuccess;
+        }
+    }
+}
