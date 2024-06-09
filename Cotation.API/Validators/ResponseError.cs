167ae61bf550860fa44d058773cf18428a8db95b
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-09 17:47:28 -0300

    Update type for send params

--- Cotation.API/Validators/ResponseError.cs
+++ Cotation.API/Validators/ResponseError.cs
@@ -2,5 +2,12 @@
     public class ResponseError {
         public string? PropertyName { get; set; }
         public string? ErrorMessage { get; set; }
+
+        public ResponseError(string PropertyName, string ErrorMessage) {
+            PropertyName = PropertyName;
+            ErrorMessage = ErrorMessage;
+        }  
+
+        public ResponseError() { }
     }
 }
