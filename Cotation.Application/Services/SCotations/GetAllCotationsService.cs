53c18d16146c377e1f57f080434478104eaad3c1
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-09 16:21:37 -0300

    Creting serve get all cotations

---
+++ Cotation.Application/Services/SCotations/GetAllCotationsService.cs
@@ -0,0 +1,25 @@
+﻿using Cotation.Communication.ModelsViews.Responses.Cotations;
+using Cotation.Infrastructure.Repositories.RepositoryCotations;
+
+namespace Cotation.Application.Services.SCotations {
+    public class GetAllCotationsService(ICotationRepository cotationRepository) {
+        private readonly ICotationRepository _cotationRepository = cotationRepository;
+
+        public async Task<List<ResponseCotations>> Execute() {
+            var cotations = await _cotationRepository.GetAll();
+
+            var listCotations = new List<ResponseCotations>();
+
+            foreach (var cotation in cotations) {
+
+                listCotations.Add(new ResponseCotations {
+                    UserId = cotation.UserId,
+                    CompanyId = cotation.CompanyId,
+                    Amount = cotation.Amount,
+                });
+
+            }
+            return listCotations;
+        }
+    }
+}
