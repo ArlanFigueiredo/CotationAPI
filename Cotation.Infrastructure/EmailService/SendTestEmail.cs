1ca28564784e74f918a13107e1ea0ac51228166d
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-10 17:05:36 -0300

    Creating service for send Email

---
+++ Cotation.Infrastructure/EmailService/SendTestEmail.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Mail;
+
+namespace Cotation.Infrastructure.EmailService {
+    public class SendTestEmail {
+        public MailMessage SenderTestEmail(string subject, string body, string to) {
+            //Attachment attachment = new("C:/Users/arlan/Downloads/Cotação Ellen.pdf");
+            MailMessage mailMessage = new() {
+                From = new MailAddress("arlan.carloz@gmail.com"),
+                Subject = subject,
+                Body = body,
+                
+            };
+            //Adicionando link do anexo ao escopo
+            //mailMessage.Attachments.Add(attachment);
+            mailMessage.To.Add(new MailAddress(to));
+            //mailMessage.To.Add(new MailAddress("anacarolinemarques18@gmail.com"));
+            return mailMessage;
+        }
+    }
+}
---
+++ Cotation.Infrastructure/EmailService/SettingsEmailService.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Cotation.Infrastructure.EmailService {
+    public class SettingsEmailService {
+
+        public async Task OnConfigurateSendEmailService(MailMessage mailMessage) {
+
+            SmtpClient smtpClient = new("smtp.gmail.com", 587) {
+                EnableSsl = true, // Habilitar SSL/TLS
+                Credentials = new NetworkCredential("arlan.carloz@gmail.com", "sznl mmll tuda pkwn")
+            };
+            smtpClient.Send(mailMessage);
+
+            Console.WriteLine("Email enviado com sucesso!");
+        }
+    }
+}
+
