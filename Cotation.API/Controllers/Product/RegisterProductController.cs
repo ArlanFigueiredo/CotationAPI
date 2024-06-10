cadcf01bafa2839e12d49d4e795eb552bf6c139d
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-10 17:37:59 -0300

    Creating controller of create product

---
+++ Cotation.API/Controllers/Product/RegisterProductController.cs
@@ -0,0 +1,35 @@
+﻿using Cotation.API.Validators.Product;
+using Cotation.Application.Services.SProduct;
+using Cotation.Communication.ModelsViews.Requests.Product;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cotation.API.Controllers.Product {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterProductController(
+        RegisterProductService registerProductService,
+        ResponseErrorRegisterProduct responseErrorRegisterProduct
+    ) : ControllerBase {
+        private readonly RegisterProductService _registerProductService = registerProductService;
+        private readonly ResponseErrorRegisterProduct _responseErrorRegisterProduct = responseErrorRegisterProduct;
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> Register([FromBody] RequestProduct request) {
+            var service = _registerProductService;
+
+            try {
+                var erros = await _responseErrorRegisterProduct.GetErrors(request);
+                if (erros.Count > 0) {
+                    return BadRequest(erros);
+                }
+                var result = await service.Execute(request);
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
