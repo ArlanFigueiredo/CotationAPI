c4614ba4e92b0e0c1db1583ee582ba1540807b30
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-10 18:11:01 -0300

    Creating controller for get product by name

---
+++ Cotation.API/Controllers/Product/GetProductByNameController.cs
@@ -0,0 +1,25 @@
+﻿using Cotation.Application.Services.SProduct;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cotation.API.Controllers.Product {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetProductByNameController(GetProductByNameService getProductByNameService) : ControllerBase {
+        private readonly GetProductByNameService _getProductByNameService = getProductByNameService;
+
+        [HttpGet("/product/name/{name}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> GetByName([FromRoute] string name) {
+            var service = _getProductByNameService;
+
+            try {
+                var result = await service.Execute(name);
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
