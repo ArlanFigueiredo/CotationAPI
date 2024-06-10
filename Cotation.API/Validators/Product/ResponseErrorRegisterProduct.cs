f93fbd1e0a4ab22ad0a16d05fd9b697222ce8bdb
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-10 17:28:05 -0300

    Creating validations for data of register product

---
+++ Cotation.API/Validators/Product/ResponseErrorRegisterProduct.cs
@@ -0,0 +1,24 @@
+﻿using Cotation.Communication.ModelsViews.Requests.Product;
+
+namespace Cotation.API.Validators.Product {
+    public class ResponseErrorRegisterProduct(ValidatorRegisterProduct validator) {
+        private readonly ValidatorRegisterProduct _validator = validator;
+
+        public async Task<List<ResponseError>> GetErrors(RequestProduct request) {
+            var validatorProduct = await _validator.ValidateAsync(request);
+
+            var erros = new List<ResponseError>();
+
+            if (!validatorProduct.IsValid) {
+                foreach (var error in validatorProduct.Errors) {
+
+                    erros.Add(new ResponseError {
+                        PropertyName = error.PropertyName,
+                        ErrorMessage = error.ErrorMessage,
+                    });
+                }
+            }
+            return erros;
+        }
+    }
+}
---
+++ Cotation.API/Validators/Product/ValidatorRegisterProduct.cs
@@ -0,0 +1,30 @@
+﻿using Cotation.Communication.ModelsViews.Requests.Product;
+using FluentValidation;
+
+namespace Cotation.API.Validators.Product {
+    public class ValidatorRegisterProduct : AbstractValidator<RequestProduct> {
+
+        [Obsolete]
+        public ValidatorRegisterProduct() {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+
+            RuleFor(product => product.Name)
+                .NotEmpty()
+                .WithMessage("O campo Nome é obrigatorio. Por favor, preencha-o");
+
+            RuleFor(product => product.Description)
+                .NotEmpty()
+                .WithMessage("O campo Descrição é obrigatorio. Por favor, preencha-o");
+
+            RuleFor(product => product.Category)
+                 .NotEmpty()
+                 .WithMessage("O campo Categoria é obrigatorio. Por favor, preencha-o");
+
+            RuleFor(product => product.PriceUnit)
+                .NotEmpty()
+                .WithMessage("O campo Preço Unico é obrigatorio. Por favor, preencha-o");
+
+        }
+    }
+}
