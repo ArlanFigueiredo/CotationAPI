91f517478b100ba3da73b96faf966b1b8c0ce7b5
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-10 18:07:30 -0300

    Creating controller for get product by id

---
+++ Cotation.API/Controllers/Product/GetProductByIdController.cs
@@ -0,0 +1,25 @@
+﻿using Cotation.Application.Services.SProduct;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cotation.API.Controllers.Product {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetProductByIdController(GetProductByIdService getProductByIdService) : ControllerBase {
+        private readonly GetProductByIdService _getProductByIdService = getProductByIdService;
+
+        [HttpGet("/product/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> GetById([FromRoute] Guid id) {
+            var service = _getProductByIdService;
+
+            try {
+                var result = await service.Execute(id);
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
