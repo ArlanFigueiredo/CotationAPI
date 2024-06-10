10b0f8ea7b45ac49a128de767519c720fa310676
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-10 18:43:35 -0300

    Creating controller for get all products

---
+++ Cotation.API/Controllers/Product/GetAllProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Cotation.Application.Services.SProduct;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cotation.API.Controllers.Product {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetAllProductsController(GetAllProductService getAllProductService) : ControllerBase {
+        private readonly GetAllProductService _getAllProductService = getAllProductService;
+
+        [HttpGet("/products")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<ActionResult> GetAll() {
+            var service = _getAllProductService;
+
+            try {
+                var result = await _getAllProductService.Execute();
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
