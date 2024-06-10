5fb0dfa1a2e04a80ae184e313d3344f143a10c45
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-10 18:02:46 -0300

    Creating controller of delete product

---
+++ Cotation.API/Controllers/Product/DeleteProductController.cs
@@ -0,0 +1,25 @@
+﻿using Cotation.Application.Services.SProduct;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cotation.API.Controllers.Product {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeleteProductController(DeleteProductService deleteProductService) : ControllerBase {
+        private readonly DeleteProductService _deleteProductService = deleteProductService;
+
+        [HttpDelete("product/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> Delete([FromRoute] Guid id) {
+            var service = _deleteProductService;
+
+            try {
+                var result = await service.Execute(id);
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
