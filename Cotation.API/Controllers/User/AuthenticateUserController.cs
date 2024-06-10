47c4bd6a7573a1682a717f7cb652025dfdb72e55
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-10 17:05:59 -0300

    testing controller with send email

--- Cotation.API/Controllers/User/AuthenticateUserController.cs
+++ Cotation.API/Controllers/User/AuthenticateUserController.cs
@@ -1,12 +1,21 @@
 ﻿using Cotation.Application.Services.SUser;
 using Cotation.Communication.ModelsViews.Requests.User;
+using Cotation.Infrastructure.EmailService;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
 
 namespace Cotation.API.Controllers.User {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthenticateUserController(AuthenticateUserService authenticateUserService) : ControllerBase {
+    public class AuthenticateUserController(
+        AuthenticateUserService authenticateUserService
+        //SettingsEmailService settingsEmailService,
+        //SendTestEmail sendTestEmail
+
+    ) : ControllerBase {
         private readonly AuthenticateUserService _authenticateUserService = authenticateUserService;
+        //private readonly SettingsEmailService _settingsEmailService = settingsEmailService;
+        //private readonly SendTestEmail _sendTestEmail = sendTestEmail;
 
         [HttpPost("/user/authenticate")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -16,6 +25,9 @@ namespace Cotation.API.Controllers.User {
             var service = _authenticateUserService;
 
             try {
+                //var resultSend = _sendTestEmail.SenderTestEmail("Você fez login", "Sua conta está autenticada!", "estudosarlan@gmail.com");
+                //await _settingsEmailService.OnConfigurateSendEmailService(resultSend);
+                
                 var result = await service.Execute(request);
                 return Ok(result);
             }
