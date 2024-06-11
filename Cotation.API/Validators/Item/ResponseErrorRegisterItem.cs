7a754d61eb031f8ab6f6efe58b0886b8ab9cf7e4
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-11 10:29:47 -0300

    Creating list of return erros of item

---
+++ Cotation.API/Validators/Item/ResponseErrorRegisterItem.cs
@@ -0,0 +1,26 @@
+﻿using Cotation.Communication.ModelsViews.Requests.Item;
+
+namespace Cotation.API.Validators.Item {
+    public class ResponseErrorRegisterItem(ValidatorRegisterItem validator) {
+        private readonly ValidatorRegisterItem _validator = validator;
+
+        public async Task<List<ResponseError>> GetErros(RequestItem request) {
+
+            var validatorItem = await _validator.ValidateAsync(request);
+
+            var error = new List<ResponseError>();
+
+            if (validatorItem.IsValid) {
+
+                foreach (var erros in validatorItem.Errors) {
+
+                    error.Add(new ResponseError {
+                        PropertyName = erros.PropertyName,
+                        ErrorMessage = erros.ErrorMessage,
+                    });
+                }
+            }
+            return error;
+        }
+    }
+}
