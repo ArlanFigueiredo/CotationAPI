77523d0a6adb1981b4f0a1caf375eda9b3bd1992
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-11 11:05:30 -0300

    Creating controller of get all items

---
+++ Cotation.API/Controllers/Item/GetAllItemsController.cs
@@ -0,0 +1,26 @@
+﻿using Cotation.Application.Services.SItem;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cotation.API.Controllers.Item {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetAllItemsController(GetAllItemService getAllItemService) : ControllerBase {
+        private readonly GetAllItemService _getAllItemsService = getAllItemService;
+
+        [HttpGet("/item")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<ActionResult> GetAllItems() {
+            var service = _getAllItemsService;
+
+            try {
+                var result = await service.Execute();
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
