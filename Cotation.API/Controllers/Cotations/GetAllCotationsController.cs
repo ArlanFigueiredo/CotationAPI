81f34d328de9229c3e39fab5ada92fec0fc7515d
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-11 12:22:02 -0300

    Creating controller of get all cotations

---
+++ Cotation.API/Controllers/Cotations/GetAllCotationsController.cs
@@ -0,0 +1,25 @@
+﻿using Cotation.Application.Services.SCotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cotation.API.Controllers.Cotations {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetAllCotationsController(GetAllCotationsService getAllCotationsService) : ControllerBase {
+        private readonly GetAllCotationsService _getAllCotationsService = getAllCotationsService;
+
+        [HttpGet("/cotations")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> GetAll() {
+            var service = _getAllCotationsService;
+
+            try {
+                var result = await service.Execute();
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
