22a2b5b694b83563d1827bfd456e065ee1f6f545
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-11 11:01:45 -0300

    Creating controller of get item by id

---
+++ Cotation.API/Controllers/Item/GetItemByIdController.cs
@@ -0,0 +1,25 @@
+﻿using Cotation.Application.Services.SItem;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cotation.API.Controllers.Item {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetItemByIdController(GetItemByIdService getItemByIdService) : ControllerBase {
+        private readonly GetItemByIdService _getItemByIdService = getItemByIdService;
+
+        [HttpGet("/item/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> GetById([FromRoute] Guid id) {
+            var service = _getItemByIdService;
+
+            try {
+                var result = await service.Execute(id);
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
