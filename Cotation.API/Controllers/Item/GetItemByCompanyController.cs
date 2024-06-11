e38193983fb80e7462cd59d1abd174a0a4fcc4ad
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-11 10:57:09 -0300

    Creating controller of get items by company

---
+++ Cotation.API/Controllers/Item/GetItemByCompanyController.cs
@@ -0,0 +1,26 @@
+﻿using Cotation.Application.Services.SItem;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cotation.API.Controllers.Item {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetItemByCompanyController(GetItemsByCompanyServices getItemsByCompanyServices) : ControllerBase {
+        private readonly GetItemsByCompanyServices _getItemsByCompanyServices = getItemsByCompanyServices;
+
+        [HttpGet("/item/companyId/{companyId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> GetByCompanyId([FromRoute] Guid companyId) {
+            var service = _getItemsByCompanyServices;
+
+            try {
+                var result = await service.Execute(companyId);
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
