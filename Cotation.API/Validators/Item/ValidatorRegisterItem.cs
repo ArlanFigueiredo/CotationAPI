98582bad71f33492825d59971d55a1f42ef0125c
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-11 10:20:12 -0300

    Creating validator for data of item

--- Cotation.API/Validators/Item/ValidatorRegisterItem.cs
+++ Cotation.API/Validators/Item/ValidatorRegisterItem.cs
@@ -1,4 +1,28 @@
-﻿namespace Cotation.API.Validators.Item {
-    public class ValidatorRegisterItem {
+﻿using Cotation.Communication.ModelsViews.Requests.Item;
+using FluentValidation;
+
+namespace Cotation.API.Validators.Item {
+    public class ValidatorRegisterItem : AbstractValidator<RequestItem> {
+
+        [Obsolete]
+        public ValidatorRegisterItem() {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(item => item.CompanyId)
+                .NotEmpty()
+                .WithMessage("Erro: Não foi identificado para quem o Item está sendo criado.");
+
+            RuleFor(item => item.ProductId)
+               .NotEmpty()
+               .WithMessage("Erro: Não foi identificado o produto.");
+
+            RuleFor(item => item.Quantity)
+               .NotEmpty()
+               .WithMessage("O campo Quantidade é obrigatorio. Por favor, preencha-o");
+
+            RuleFor(item => item.Quantity)
+               .GreaterThan(0)
+               .WithMessage("A quantidade deve ser pelo menos 1");
+        }
     }
 }
