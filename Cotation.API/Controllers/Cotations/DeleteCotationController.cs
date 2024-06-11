7c449230d02d8a44a693adf3e0c542e104db8448
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-11 11:59:04 -0300

    Creating controller of delete cotation

---
+++ Cotation.API/Controllers/Cotations/DeleteCotationController.cs
@@ -0,0 +1,25 @@
+﻿using Cotation.Application.Services.SCotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cotation.API.Controllers.Cotations {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeleteCotationController(DeleteCotationSerivce deleteCotationSerivce) : ControllerBase {
+        private readonly DeleteCotationSerivce _deleteCotationService = deleteCotationSerivce;
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> Delete([FromRoute] Guid id) {
+            var service = _deleteCotationService;
+
+            try {
+                var result = await service.Execute(id);
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
