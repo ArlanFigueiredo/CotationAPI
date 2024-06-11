10fdddde2010de2b5eca6e7b99a6c7bf6f3373d8
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-11 10:34:29 -0300

    Creating controller of register item

---
+++ Cotation.API/Controllers/Item/RegisterItemController.cs
@@ -0,0 +1,35 @@
+﻿using Cotation.API.Validators.Item;
+using Cotation.Application.Services.SItem;
+using Cotation.Communication.ModelsViews.Requests.Item;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cotation.API.Controllers.Item {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterItemController(
+        RegisterItemService registerItemService,
+        ResponseErrorRegisterItem responseErrorRegisterItem
+    ) : ControllerBase {
+        private readonly RegisterItemService _registerItemService = registerItemService;
+        private readonly ResponseErrorRegisterItem _responseErrorRegisterItem = responseErrorRegisterItem;
+        [HttpPost("/item")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> Register([FromBody] RequestItem request) {
+            var service = _registerItemService;
+
+            try {
+                var erros = await _responseErrorRegisterItem.GetErros(request);
+                if (erros.Count > 0) {
+                    return BadRequest(erros);
+                }
+                var result = await service.Execute(request);
+                return Ok(result);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
+
