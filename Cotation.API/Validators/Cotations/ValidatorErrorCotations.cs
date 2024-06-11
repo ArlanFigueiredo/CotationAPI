a4a02d9d7656e88654471e556db3d5910a138b06
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-11 11:51:22 -0300

    Creating validator for data of cotation

---
+++ Cotation.API/Validators/Cotations/ValidatorErrorCotations.cs
@@ -0,0 +1,21 @@
+﻿using Cotation.Communication.ModelsViews.Requests.Cotations;
+using FluentValidation;
+
+namespace Cotation.API.Validators.Cotations {
+    public class ValidatorErrorCotations : AbstractValidator<RequestCotations> {
+
+        [Obsolete]
+        public ValidatorErrorCotations() {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(cotation => cotation.UserId)
+                .NotEmpty()
+                .WithMessage("Erro: não foi identificado quem está fazendo a cotação.");
+
+            RuleFor(cotation => cotation.CompanyId)
+                .NotEmpty()
+                .WithMessage("Erro: não foi identificado para qual empresa está fazendo a cotação.");
+
+        }
+    }
+}
