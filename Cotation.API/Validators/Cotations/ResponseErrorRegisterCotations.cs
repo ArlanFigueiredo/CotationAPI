cb6a1f2f0f147558b7bb56a608e03c2ca69e1f6b
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-11 11:54:01 -0300

    Creating service of return list erros of relationships in cotations

---
+++ Cotation.API/Validators/Cotations/ResponseErrorRegisterCotations.cs
@@ -0,0 +1,24 @@
+﻿using Cotation.Communication.ModelsViews.Requests.Cotations;
+
+namespace Cotation.API.Validators.Cotations {
+    public class ResponseErrorRegisterCotations(ValidatorErrorCotations validator) {
+        private readonly ValidatorErrorCotations _validator = validator;
+
+        public async Task<List<ResponseError>> GetErros(RequestCotations request) {
+            var validatorCotations = await _validator.ValidateAsync(request);
+
+            var erros = new List<ResponseError>();
+
+            if (!validatorCotations.IsValid) {
+
+                foreach (var error in validatorCotations.Errors) {
+                    erros.Add(new ResponseError {
+                        PropertyName = error.PropertyName,
+                        ErrorMessage = error.ErrorMessage,
+                    });
+                }
+            }
+            return erros;
+        }
+    }
+}
