bae3ea423dfe6d067c50b759c2459cc277b03b19
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-11 11:14:26 -0300

    Creating service of delete item by id

---
+++ Cotation.Application/Services/SItem/DeleteItemService.cs
@@ -0,0 +1,13 @@
+﻿using Cotation.Infrastructure.Repositories.RepositoryItem;
+
+namespace Cotation.Application.Services.SItem {
+    internal class DeleteItemService(IItemsRepository itemsRepository) {
+        private readonly IItemsRepository _itemsRepository = itemsRepository;
+        private readonly string MessageSuccess = "Item deletado com sucesso.";
+        public async Task<string> Execute(Guid id) {
+            var item = await _itemsRepository.GetById(id) ?? throw new Exception("Item não encontrado.");
+            await _itemsRepository.Delete(item);
+            return MessageSuccess;
+        }
+    }
+}
