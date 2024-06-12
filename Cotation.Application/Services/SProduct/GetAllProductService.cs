8cce144b092063250f6aa1fd23fa200fd4ff6f11
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-12 15:52:41 -0300

    Implemented interface

--- Cotation.Application/Services/SProduct/GetAllProductService.cs
+++ Cotation.Application/Services/SProduct/GetAllProductService.cs
@@ -1,8 +1,9 @@
-﻿using Cotation.Communication.ModelsViews.Responses.Product;
+﻿using Cotation.Application.Interfaces.Product;
+using Cotation.Communication.ModelsViews.Responses.Product;
 using Cotation.Infrastructure.Repositories.RepositoryProduct;
 
 namespace Cotation.Application.Services.SProduct {
-    public class GetAllProductService(IProductRepository productRepository) {
+    public class GetAllProductService(IProductRepository productRepository) : IGetAllProductService {
         private readonly IProductRepository _productRepository = productRepository;
 
         public async Task<List<ResponseProduct>> Execute() {
