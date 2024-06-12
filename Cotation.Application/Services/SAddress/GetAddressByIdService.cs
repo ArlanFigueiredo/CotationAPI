8922a8f459889db1baab0adf941f3e83508a1b84
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-12 15:34:22 -0300

    Implemented interface

--- Cotation.Application/Services/SAddress/GetAddressByIdService.cs
+++ Cotation.Application/Services/SAddress/GetAddressByIdService.cs
@@ -1,12 +1,21 @@
-﻿using Cotation.Domain.Entities;
+﻿using Cotation.Application.Interfaces.Address;
+using Cotation.Communication.ModelsViews.Responses.Address;
 using Cotation.Infrastructure.Repositories.RepositoryAddress;
 
 namespace Cotation.Application.Services.SAddress {
-    public class GetAddressByIdService(IAddressRepository addressRepository) {
+    public class GetAddressByIdService(IAddressRepository addressRepository) : IGetAddressByIdService {
         private readonly IAddressRepository _addressRepository = addressRepository;
-        public async Task<Address> Execute(Guid id) {
+        public async Task<ResponseAddress> Execute(Guid id) {
             var address = await _addressRepository.GetById(id) ?? throw new Exception("Empresa não existe.");
-            return address;
+            return new ResponseAddress {
+                CompanyId = address.Id,
+                ZipCode = address.ZipCode,
+                Road = address.Road,
+                Number = address.Number,
+                NeighBorHood = address.NeighBorHood,
+                City = address.City,
+                State = address.State,
+            };
         }
     }
 }
