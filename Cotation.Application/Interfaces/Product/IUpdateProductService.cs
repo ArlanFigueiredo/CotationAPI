ce4deb876c5757936aac84872525e12f3284870d
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-12 15:56:00 -0300

    Creating interface for service of updated product

---
+++ Cotation.Application/Interfaces/Product/IUpdateProductService.cs
@@ -0,0 +1,8 @@
+﻿using Cotation.Communication.ModelsViews.Requests.Product;
+using Cotation.Communication.ModelsViews.Responses.Product;
+
+namespace Cotation.Application.Interfaces.Product {
+    public interface IUpdateProductService {
+        public Task<ResponseProduct> Execute(RequestProduct request, Guid id);
+    }
+}
