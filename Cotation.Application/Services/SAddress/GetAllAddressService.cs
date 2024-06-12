086f1eeee8fb4d9d7f600ff4bdfc0ff2967f2f5b
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-12 15:34:36 -0300

    Implemented interface

--- Cotation.Application/Services/SAddress/GetAllAddressService.cs
+++ Cotation.Application/Services/SAddress/GetAllAddressService.cs
@@ -1,12 +1,27 @@
-﻿using Cotation.Domain.Entities;
+﻿using Cotation.Application.Interfaces.Address;
+using Cotation.Communication.ModelsViews.Responses.Address;
 using Cotation.Infrastructure.Repositories.RepositoryAddress;
 
 namespace Cotation.Application.Services.SAddress {
-    public class GetAllAddressService(IAddressRepository addressRepository) {
+    public class GetAllAddressService(IAddressRepository addressRepository) : IGetAllAddressService {
         private readonly IAddressRepository _addressRepository = addressRepository;
-        public async Task<List<Address>> Execute() {
-            return await _addressRepository.GetAll();
-        }
+        public async Task<List<ResponseAddress>> Execute() {
+            var addresss = await _addressRepository.GetAll();
 
+            var addressList = new List<ResponseAddress>();
+
+            foreach (var address in addresss) {
+                addressList.Add(new ResponseAddress {
+                    CompanyId = address.Id,
+                    ZipCode = address.ZipCode,
+                    Road = address.Road,
+                    Number = address.Number,
+                    NeighBorHood = address.NeighBorHood,
+                    City = address.City,
+                    State = address.State,
+                });
+            }
+            return addressList;
+        }
     }
 }
