6f080952cbb21f5638a6295f3fff049130f1eca8
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-12 15:55:21 -0300

    Creating interface for service of get product by name

---
+++ Cotation.Application/Interfaces/Product/IGetProductByNameService.cs
@@ -0,0 +1,7 @@
+﻿using Cotation.Communication.ModelsViews.Responses.Product;
+
+namespace Cotation.Application.Interfaces.Product {
+    public interface IGetProductByNameService {
+        public Task<ResponseProduct> Execute(string name);
+    }
+}
