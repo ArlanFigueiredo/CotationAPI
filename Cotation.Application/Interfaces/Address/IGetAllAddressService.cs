2d82bc5de92cf4fbdd0075f70f1a914f697abc00
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-12 15:39:07 -0300

    Creating interface for service of get all address

---
+++ Cotation.Application/Interfaces/Address/IGetAllAddressService.cs
@@ -0,0 +1,7 @@
+﻿using Cotation.Communication.ModelsViews.Responses.Address;
+
+namespace Cotation.Application.Interfaces.Address {
+    public interface IGetAllAddressService {
+        public Task<List<ResponseAddress>> Execute();
+    }
+}
