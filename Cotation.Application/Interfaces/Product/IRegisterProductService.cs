3673c8b40135e6dae7e7f7c12d21f98b84b4730a
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-12 15:55:32 -0300

    Creating interface for service of register product

---
+++ Cotation.Application/Interfaces/Product/IRegisterProductService.cs
@@ -0,0 +1,8 @@
+﻿using Cotation.Communication.ModelsViews.Requests.Product;
+using Cotation.Communication.ModelsViews.Responses.Product;
+
+namespace Cotation.Application.Interfaces.Product {
+    public interface IRegisterProductService {
+        public Task<ResponseProduct> Execute(RequestProduct request);
+    }
+}
