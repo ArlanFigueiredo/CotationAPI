e4e76ce71b7bdde8c58403947d7c585b2be168f1
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-12 15:55:11 -0300

    Creating interface for service of get product by id

---
+++ Cotation.Application/Interfaces/Product/IGetProductByIdService.cs
@@ -0,0 +1,7 @@
+﻿using Cotation.Communication.ModelsViews.Responses.Product;
+
+namespace Cotation.Application.Interfaces.Product {
+    public interface IGetProductByIdService {
+        public Task<ResponseProduct> Execute(Guid id);
+    }
+}
