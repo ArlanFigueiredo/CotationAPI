f168b8743b6f3a08219e7388d57b88281c334c83
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-12 15:38:52 -0300

    Creating interface for service of get address by id

---
+++ Cotation.Application/Interfaces/Address/IGetAddressByIdService.cs
@@ -0,0 +1,7 @@
+﻿using Cotation.Communication.ModelsViews.Responses.Address;
+
+namespace Cotation.Application.Interfaces.Address {
+    public interface IGetAddressByIdService {
+        public Task<ResponseAddress> Execute(Guid id);
+    }
+}
