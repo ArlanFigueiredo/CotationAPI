4e942a0d608bb0fd3faf03c50f79e8d0f7ef975e
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-12 15:33:10 -0300

    Implemented interface

--- Cotation.Application/Services/SAddress/DeleteAddressService.cs
+++ Cotation.Application/Services/SAddress/DeleteAddressService.cs
@@ -1,7 +1,8 @@
-﻿using Cotation.Infrastructure.Repositories.RepositoryAddress;
+﻿using Cotation.Application.Interfaces.Address;
+using Cotation.Infrastructure.Repositories.RepositoryAddress;
 
 namespace Cotation.Application.Services.SAddress {
-    public class DeleteAddressService(IAddressRepository addressRepository) {
+    public class DeleteAddressService(IAddressRepository addressRepository) : IDeleteAddressService {
         private readonly IAddressRepository _addressRepository = addressRepository;
         private readonly string MessageSuccess = "Endereço deletado com sucesso";
         public async Task<string> Execute(Guid id) {
