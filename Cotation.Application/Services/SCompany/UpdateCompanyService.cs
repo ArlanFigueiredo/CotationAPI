2ea239859274332332ac403ffadf4a574981bf27
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-13 15:01:34 -0300

    implemented interface

--- Cotation.Application/Services/SCompany/UpdateCompanyService.cs
+++ Cotation.Application/Services/SCompany/UpdateCompanyService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Cotation.Application.Interfaces.Company;
 using Cotation.Communication.DTOS.CompanyDTO;
 using Cotation.Communication.ModelsViews.Requests.Company;
 using Cotation.Communication.ModelsViews.Responses.Company;
@@ -6,7 +7,7 @@ using Cotation.Domain.Entities;
 using Cotation.Infrastructure.Repositories.RepositoryCompany;
 
 namespace Cotation.Application.Services.SCompany {
-    public class UpdateCompanyService(ICompanyRepository companyRepository, IMapper mapper) {
+    public class UpdateCompanyService(ICompanyRepository companyRepository, IMapper mapper) : IUpdateCompanyService {
         private readonly ICompanyRepository _companyRepository = companyRepository;
         private readonly IMapper _mapper = mapper;
 
