cf28e47a3e7a3d42d156746ead35fbea2b9e5641
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-13 13:42:51 -0300

    Company delete service abstraction created

--- Cotation.Application/Services/SUser/AuthenticateUserService.cs
+++ Cotation.Application/Services/SUser/AuthenticateUserService.cs
@@ -1,13 +1,15 @@
-﻿using Cotation.Application.Utilities.User;
+﻿using Cotation.Application.Interfaces.User;
+using Cotation.Application.Utilities.User;
 using Cotation.Communication.ModelsViews.Requests.User;
 using Cotation.Infrastructure.Repositories.RepositoryUser;
 
-namespace Cotation.Application.Services.SUser {
+namespace Cotation.Application.Services.SUser
+{
     public class AuthenticateUserService(
         IUserRepository userRepository,
         VerifyHashPassword verifyHashPassword,
         GenerateTokenJWT generateTokenJWT
-    ) {
+    ) : IAuthenticateUserService {
         private readonly IUserRepository _userRepository = userRepository;
         private readonly VerifyHashPassword _verifyHashPassword = verifyHashPassword;
         private readonly GenerateTokenJWT _generateTokenJWT = generateTokenJWT;
@@ -16,7 +18,7 @@ namespace Cotation.Application.Services.SUser {
             var user = await _userRepository.GetByEmail(login.Email) ?? throw new Exception("Credenciais invalida. Por favor, verifique e tente novamente.");
 
             var authenticated = _verifyHashPassword.VerifyPasswordHash(user.Password, user.Salt, login.Password);
-                if (authenticated == false) {
+            if (authenticated == false) {
                 throw new Exception("Credenciais invalidas. Por favor, verifique e tente novamente.");
             }
 
