39306ce193eb0a68b5a99cdbb197cc4b7ed883a0
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-13 15:00:11 -0300

    Implemented interface

--- Cotation.Application/Services/SCompany/GetAllCompanyService.cs
+++ Cotation.Application/Services/SCompany/GetAllCompanyService.cs
@@ -1,11 +1,27 @@
-﻿using Cotation.Domain.Entities;
+﻿using Cotation.Application.Interfaces.Company;
+using Cotation.Communication.ModelsViews.Responses.Company;
 using Cotation.Infrastructure.Repositories.RepositoryCompany;
 
 namespace Cotation.Application.Services.SCompany {
-    public class GetAllCompanyService(ICompanyRepository companyRepository) {
+    public class GetAllCompanyService(ICompanyRepository companyRepository) : IGetAllCompanyService {
         private readonly ICompanyRepository _companyRepository = companyRepository;
-        public async Task<List<Company>> Execute() {
-            return await _companyRepository.GetAll();
+
+        public async Task<List<ResponseCompany>> Execute() {
+            var companys = await _companyRepository.GetAll();
+
+            var listCompany = new List<ResponseCompany>();
+
+            foreach (var company in companys) {
+
+                listCompany.Add(new ResponseCompany {
+                    Id = company.Id,
+                    Name = company.Name,
+                    Cnpj = company.Cnpj,
+                    Phone = company.Phone,
+                    Email = company.Email,
+                });
+            }
+            return listCompany;
         }
     }
 }
