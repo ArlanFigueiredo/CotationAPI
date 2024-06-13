97ab68f651503dd4567063220c30d4843ae9f632
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-13 14:58:28 -0300

    Creating interface for service of register company

---
+++ Cotation.Application/Interfaces/Company/IRegisterCompanyService.cs
@@ -0,0 +1,8 @@
+﻿using Cotation.Communication.ModelsViews.Requests.Company;
+using Cotation.Communication.ModelsViews.Responses.Company;
+
+namespace Cotation.Application.Interfaces.Company {
+    public interface IRegisterCompanyService {
+        public Task<ResponseCompany> Execute(RequestCompany request);
+    }
+}
