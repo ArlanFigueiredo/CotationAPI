8854141a8921c5a77045b6aa40c8d8b0bbef9188
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-13 14:56:53 -0300

    Creating interface of service  get all companies

---
+++ Cotation.Application/Interfaces/Company/IGetAllCompanyService.cs
@@ -0,0 +1,7 @@
+﻿using Cotation.Communication.ModelsViews.Responses.Company;
+
+namespace Cotation.Application.Interfaces.Company {
+    public interface IGetAllCompanyService {
+        public Task<List<ResponseCompany>> Execute();
+    }
+}
