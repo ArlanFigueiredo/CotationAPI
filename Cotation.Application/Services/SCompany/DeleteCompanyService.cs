b9a27bb5892ce3ef6aa85609a25585ef41776cdd
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-13 13:44:51 -0300

    company delete interface implemented

--- Cotation.Application/Services/SCompany/DeleteCompanyService.cs
+++ Cotation.Application/Services/SCompany/DeleteCompanyService.cs
@@ -1,7 +1,8 @@
-﻿using Cotation.Infrastructure.Repositories.RepositoryCompany;
+﻿using Cotation.Application.Interfaces.Company;
+using Cotation.Infrastructure.Repositories.RepositoryCompany;
 
 namespace Cotation.Application.Services.SCompany {
-    public class DeleteCompanyService(ICompanyRepository companyRepository) {
+    public class DeleteCompanyService(ICompanyRepository companyRepository) : IDeleteCompanyService {
         private readonly ICompanyRepository _companyRepository = companyRepository;
         private readonly string MessageSuccess = "Empresa deletada com sucesso.";
         public async Task<string> Execute(Guid id) {
