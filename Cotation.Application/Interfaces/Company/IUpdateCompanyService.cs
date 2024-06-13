f75bf24215a84a1c873bec4f4945d352c75f0515
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-13 14:58:50 -0300

    Creating interface for service of update company

---
+++ Cotation.Application/Interfaces/Company/IUpdateCompanyService.cs
@@ -0,0 +1,8 @@
+﻿using Cotation.Communication.ModelsViews.Requests.Company;
+using Cotation.Communication.ModelsViews.Responses.Company;
+
+namespace Cotation.Application.Interfaces.Company {
+    public interface IUpdateCompanyService {
+        public Task<ResponseCompany> Execute(RequestCompany request, Guid id);
+    }
+}
