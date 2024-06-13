e8186a16d9ab7eea892f9b764de0d08db94cb9e7
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-13 14:57:32 -0300

    Creating inter face of service for get company by id

---
+++ Cotation.Application/Interfaces/Company/IGetCompanyByIdService.cs
@@ -0,0 +1,7 @@
+﻿using Cotation.Communication.ModelsViews.Responses.Company;
+
+namespace Cotation.Application.Interfaces.Company {
+    public interface IGetCompanyByIdService {
+        public Task<ResponseCompany> Execute(Guid id);
+    }
+}
