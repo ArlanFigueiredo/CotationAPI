ecaf7b1d07daecdbcc4622fb8fdae1092956aa80
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-13 14:58:02 -0300

    Creating interface for service of get company by name

---
+++ Cotation.Application/Interfaces/Company/IGetCompanyByNameService.cs
@@ -0,0 +1,8 @@
+﻿using Cotation.Communication.ModelsViews.Responses.Company;
+
+namespace Cotation.Application.Interfaces.Company {
+    public interface IGetCompanyByNameService {
+
+        public Task<ResponseCompany> Execute(string name);
+    }
+}
