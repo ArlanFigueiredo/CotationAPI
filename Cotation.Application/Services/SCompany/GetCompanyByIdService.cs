116dc63d6fbfa898b1637f0aa88d5f78a19a2b9f
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-13 15:00:40 -0300

    implemented interface

--- Cotation.Application/Services/SCompany/GetCompanyByIdService.cs
+++ Cotation.Application/Services/SCompany/GetCompanyByIdService.cs
@@ -1,12 +1,18 @@
-﻿using Cotation.Domain.Entities;
+﻿using Cotation.Application.Interfaces.Company;
+using Cotation.Communication.ModelsViews.Responses.Company;
+using Cotation.Domain.Entities;
 using Cotation.Infrastructure.Repositories.RepositoryCompany;
 
 namespace Cotation.Application.Services.SCompany {
-    public class GetCompanyByIdService(ICompanyRepository companyRepository) {
+    public class GetCompanyByIdService(ICompanyRepository companyRepository) : IGetCompanyByIdService {
         private readonly ICompanyRepository _companyRepository = companyRepository;
         public async Task<Company> Execute(Guid id) {
             var companyExists = await _companyRepository.GetById(id) ?? throw new Exception("Empresa não existe.");
             return companyExists;
+        }
+
+        Task<ResponseCompany> IGetCompanyByIdService.Execute(Guid id) {
+            throw new NotImplementedException();
         }
     }
 }
